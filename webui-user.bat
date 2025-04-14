a996c705c43b8e7d781ba30ecf0186a664c97ad9
vitasnay <zernovoz26@gmail.com>
2025-04-15 00:10:51 +0300

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,8 @@
 @echo off
 
-set PYTHON=
+set PYTHON="C:\Users\Юзер\AppData\Local\Programs\Python\Python310\python.exe"
+set VENV_DIR=venv
 set GIT=
-set VENV_DIR=
 set COMMANDLINE_ARGS=
 
 call webui.bat
